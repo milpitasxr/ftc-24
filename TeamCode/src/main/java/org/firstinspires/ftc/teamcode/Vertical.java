966afd233d917115b0391360b35f81bf2fc1111b
Aryan Patel <aryan.patel1003@outlook.com>
2024-10-28 17:50:54 -0700

    Revert "Define references to servos and motors"
    This reverts commit ac3a840fa2908d57611736e1c8411de7902cbf80.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vertical.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vertical.java
@@ -1,80 +1,40 @@
 package org.firstinspires.ftc.teamcode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Vertical {
-    
-    Servo armLeft;
-    Servo armRight;
-    Servo verticalLeft;
-    Servo verticalRight;
-    Servo claw;
-    Servo rotate;
-
-    DcMotor vMotor1;
-    DcMotor vMotor2;
-    ElapsedTime vTimer;
-    String vAction;
+    ElapsedTime bucketTimer;
+    String bucketAction;
 
     public Vertical() {
-        vTimer = new ElapsedTime();
-        vAction = "RETRACT";
-
-       armLeft =  hardwareMap.get(Servo.class, "al");
-       armRight =  hardwareMap.get(Servo.class, "ar");
-       verticalLeft =  hardwareMap.get(Servo.class, "vl");
-       verticalRight =  hardwareMap.get(Servo.class, "vr");
-       claw =  hardwareMap.get(Servo.class, "claw");
-       rotate =  hardwareMap.get(Servo.class, "rotate");
-
-       //map this properly:
-       //vMotor1 = hardwareMap.get(DcMotor.class, "vMotor1");
-       //vMotor2 = hardwareMap.get(DcMotor.class, "vMotor2");
-
-
-
+        bucketTimer = new ElapsedTime();
+        bucketAction = "RETRACT";
     }
-    
-    public void Update(){
-        changeVAction();
-        if(vAction=="RETRACT"){
-            retract();
-        }
-    }
-    
-    private void retract(){
-//      WIP: Function to retract the vertical arm
-    }
-    
     public void changeVAction() {
-        if (gamepad1.b && gamepad1.dpad_right && vAction != "RETRACT") {
-            vAction = "RETRACT";
-            vTimer.reset();
-        } else if (gamepad1.b && gamepad1.dpad_left && vAction != "LOWER") {
-            vAction = "LOWER";
-            vTimer.reset();
-        } else if (gamepad1.b && gamepad1.dpad_up && vAction != "UPPER") {
-            vAction = "UPPER";
-            vTimer.reset();
-        } else if (gamepad1.b && gamepad1.dpad_down && vAction != "DOWN") {
-            vAction = "DOWN";
-            vTimer.reset();
-        } else if (gamepad1.y && gamepad1.dpad_up && vAction != "TOP") {
-            vAction = "TOP";
-            vTimer.reset();
-        } else if (gamepad1.y && gamepad1.dpad_left && vAction != "MID") {
-            vAction = "MID";
-            vTimer.reset();
-        } else if (gamepad1.y && gamepad1.dpad_down && vAction != "PICK") {
-            vAction = "PICK";
-            vTimer.reset();
-        } else if (gamepad1.y && gamepad1.dpad_right && vAction != "PLACE") {
-            vAction = "PLACE";
-            vTimer.reset();
+        if (gamepad1.b && gamepad1.dpad_right && bucketAction != "RETRACT") {
+            bucketAction = "RETRACT";
+            bucketTimer.reset();
+        } else if (gamepad1.b && gamepad1.dpad_left && bucketAction != "LOWER") {
+            bucketAction = "LOWER";
+            bucketTimer.reset();
+        } else if (gamepad1.b && gamepad1.dpad_up && bucketAction != "UPPER") {
+            bucketAction = "UPPER";
+            bucketTimer.reset();
+        } else if (gamepad1.b && gamepad1.dpad_down && bucketAction != "DOWN") {
+            bucketAction = "DOWN";
+            bucketTimer.reset();
+        } else if (gamepad1.y && gamepad1.dpad_up && bucketAction != "TOP") {
+            bucketAction = "TOP";
+            bucketTimer.reset();
+        } else if (gamepad1.y && gamepad1.dpad_left && bucketAction != "MID") {
+            bucketAction = "MID";
+            bucketTimer.reset();
+        } else if (gamepad1.y && gamepad1.dpad_down && bucketAction != "PICK") {
+            bucketAction = "PICK";
+            bucketTimer.reset();
+        } else if (gamepad1.y && gamepad1.dpad_right && bucketAction != "PLACE") {
+            bucketAction = "PLACE";
+            bucketTimer.reset();
         }
     }
 }
